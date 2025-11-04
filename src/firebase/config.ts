c1c5930e39805b85f7001b30e4866680f0f87a5c
Prerana Kailash <shecodesherway@gmail.com>
2025-11-04 08:25:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2560173527-17dab",
+  "appId": "1:371613558047:web:7b5ab7b8086a5b2ae2bc2a",
+  "apiKey": "AIzaSyCGcMaAoxEaQRTUJXEDTDfOwbcGxXnK_HQ",
+  "authDomain": "studio-2560173527-17dab.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "371613558047"
+};
