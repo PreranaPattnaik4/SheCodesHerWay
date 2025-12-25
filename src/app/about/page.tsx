bb873a6e9862ac1a02be55e47df0b51c0e314e45
Prerana Kailash <shecodesherway@gmail.com>
2025-12-25 06:26:50 +0000

    very little move to center

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -52,7 +52,7 @@ export default function AboutPage() {
                             
                             {/* Card Content */}
                             <div className="p-8 md:p-12">
-                                <div className="grid md:grid-cols-2 gap-8 items-start">
+                                <div className="grid md:grid-cols-2 gap-8 items-center">
                                     <div className="relative">
                                         <h1 className="font-headline text-9xl font-bold text-foreground">
                                             About
