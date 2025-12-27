02d3b8e932cc6ddb370ce0a2f0d12a3bb43850cf
Prerana Kailash <shecodesherway@gmail.com>
2025-12-27 19:55:33 +0000

    Search
    Live tail
    GMT+5:30
    
    ==> Downloading cache...
    ==> Cloning from

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2560173527-17dab",
   "appId": "1:371613558047:web:7b5ab7b8086a5b2ae2bc2a",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "apiKey": "AIzaSyCdv5pTx3iNWevAey9rpn5G6HEUF4b4m_8",
   "authDomain": "studio-2560173527-17dab.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "371613558047"
