d69a140413cd1edffb3a0e493288ba8bcabe1360
Prerana Kailash <shecodesherway@gmail.com>
2025-12-27 14:07:51 +0000

    ADD 10 QUERIES,CHOOSE FROM EACH SECTION OF FAQ PAGE

--- src/lib/home-faq-data.ts
+++ src/lib/home-faq-data.ts
@@ -3,8 +3,13 @@ import { faqData, type FAQ } from './faq-data';
 
 export const homeFaqData: FAQ[] = [
     faqData[0].questions[0], // What is SheCodesHerWay?
+    faqData[0].questions[1], // What is the mission and vision of SheCodesHerWay?
     faqData[1].questions[0], // What is the 'Sangini Udaan : EmpowerFly' program?
-    faqData[1].questions[2], // Is there a fee to enroll in the Sangini Udaan : EmpowerFly program?
+    faqData[1].questions[8], // Is there a fee to enroll in the Sangini Udaan : EmpowerFly program?
+    faqData[2].questions[1], // What is an 'Early Core Contributor'?
     faqData[2].questions[2], // Are the internship and contributor roles paid?
     faqData[3].questions[0], // What is the EmpowerFly Assistant?
+    faqData[3].questions[2], // What is the EmpowerFly AI Coach?
+    faqData[4].questions[0], // What personal information do you collect?
+    faqData[4].questions[2], // Is my information shared with third parties?
 ];
