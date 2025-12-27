6b75fa6afb2775573de0018360f8d26caae5d319
Prerana Kailash <shecodesherway@gmail.com>
2025-12-27 14:17:49 +0000

    MAKE 15 QUEARIES IN HOME PAGE

--- src/lib/home-faq-data.ts
+++ src/lib/home-faq-data.ts
@@ -3,13 +3,18 @@ import { faqData, type FAQ } from './faq-data';
 
 export const homeFaqData: FAQ[] = [
     faqData[0].questions[0], // What is SheCodesHerWay?
-    faqData[1].questions[0], // What is the 'Sangini Udaan : EmpowerFly' program?
+    faqData[0].questions[1], // What is the mission and vision of SheCodesHerWay?
     faqData[1].questions[1], // Who is the 'Sangini Udaan : EmpowerFly' program for?
     faqData[1].questions[4], // Is prior experience required to enroll in a specific track?
     faqData[1].questions[5], // Can someone with a non-technical background join a technical track like 'Women in Tech'?
     faqData[1].questions[10], // Is there a fee to enroll in the Sangini Udaan : EmpowerFly program?
+    faqData[1].questions[6], // Do I need to know how to code to join?
     faqData[2].questions[1], // What is an 'Early Core Contributor'?
     faqData[2].questions[2], // Are the internship and contributor roles paid?
+    faqData[2].questions[8], // Will collaboration lead to a paid job?
     faqData[3].questions[0], // What is the EmpowerFly Assistant?
+    faqData[3].questions[1], // What is the Executive Summary Generator?
+    faqData[3].questions[7], // Why are some AI tools marked as 'Coming Soon'?
+    faqData[4].questions[0], // What personal information do you collect?
     faqData[4].questions[2], // Is my information shared with third parties?
 ];
